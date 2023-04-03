93c3099c73fdbce4d77f3ad928657b4b5cac68bb
Jackie <jackieguo126@gmail.com>
2023-04-03 14:56:33 +0800

    Upload

---
+++ ConsoleApp1/ConsoleApp1/Post.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Post
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+
+        [JsonProperty("title")]
+        public string title { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://my-json-server.typicode.com/typicode/demo/posts";
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                //GET: GetAsync sends GET request to the specified URI as an asynchronous operation
+                var httpRespondMessage = await client.GetAsync(url);
+                string jsonResponse = await httpRespondMessage.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonResponse);
+
+                //DESERIALIZE json string into Post objects
+                var jsonPost = JsonConvert.DeserializeObject<Post[]>(jsonResponse);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            finally { client.Dispose(); }
+        }
+    }
+    
+}
